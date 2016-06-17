925ccc51f6b0ae65cc244b434941c30c99ebd041
jackyhobingo <jackyhobingo@gmail.com>
2016-06-18 07:21:20 +0800

    Add new story for function test that a new guy Francis come to website

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -40,9 +40,12 @@ class NewVisitorTest(LiveServerTestCase):
         # 他的興趣是綁蒼蠅魚餌
         inputbox.send_keys('Buy peacock feathers')
 
-        # 當他按下Enter時, 網頁會耕興,現在網頁列出
-        # "1. 購買孔雀羽毛" 一個待辦事項清單項目
+        # 當他按下Enter時,他會被帶到一個新的URL 
+        # 現在網頁列出 "1. Buy peacock feathers" 
+        # 一個待辦事項清單項目
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # 此時仍然有一個文字方塊讓他可以加入一個項目
@@ -52,17 +55,40 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # 網頁再次更新 現在他的清單上有這兩個項目
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers to make a fly',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        # Edith 不知道網站能否記得他的清單
-        # 接著他看到網站產生一個唯一的URL給他
-        # 網頁有一些文字說明這個效果
+        # 在新的使用者Francis 來到網站
+
+        # 我們使用一個新的瀏覽器工作階段來確保
+        ## Edith 的任何資訊都不會被cookies 等機制送出
+        self.browser.quit()
+        self.browser.webdriver.Firefox()
+
+        # Francis 造訪首頁 沒有任何Edith清單的跡象
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a flys', page_text)
+
+        # Francis 輸入一個新的項目 做出一個新的清單
+        # 他比Edith 無趣...
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Francis 取得他自己獨一無二的URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # 同樣的，沒有Edith的清單的任何跡象
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # 他很滿意，都會去睡覺了
+
         self.fail('Finish the test!')
 
         # 他前往那個URL 他的待辦清單仍然在那裡
