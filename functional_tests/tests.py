ee761191c8cb5e09b363ecb4d7897d9fa1c0554f
jackyhobingo <jackyhobingo@gmail.com>
2016-07-02 18:38:47 +0800

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,5 @@
 # Functional test
+import sys
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
@@ -7,6 +8,21 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return 
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
+
 
     def setUp(self):
         self.browser = webdriver.Chrome()
@@ -23,7 +39,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith heard a new cool todo app.
         # She went to look its home page.
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.server_url)
         self.browser.get(self.live_server_url)
 
         # 他發現首頁標題與標頭顯示待辦事項
@@ -69,7 +85,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser = webdriver.Chrome()
 
         # Francis 造訪首頁 沒有任何Edith清單的跡象
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a flys', page_text)
@@ -100,7 +116,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # Edith 前往首頁
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
         
         # 他發現輸入方塊已被妥善地置中
