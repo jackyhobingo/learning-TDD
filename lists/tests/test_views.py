2b1a607d956e277deaecc26d12bd5937212d08fb
jackyhobingo <jackyhobingo@gmail.com>
2016-07-18 17:03:08 +0800

    Implement duplicate item validation at view layer

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,5 +1,3 @@
-from unittest import skip
-
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
@@ -8,7 +6,10 @@ from django.utils.html import escape
 
 from lists.models import Item, List
 from lists.views import home_page
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (
+    DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR,
+    ExistingListItemForm, ItemForm,
+)
 
 
 class HomePageTest(TestCase):
@@ -121,7 +122,7 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get('/lists/%d/' % (list_.id,))
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
 
@@ -143,20 +144,20 @@ class ListViewTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
-    @skip
-    def test_duplicate_item_validation_error_on_page(self):
+
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
         response = self.client.post(
             '/lists/%d/' % (list1.id,),
             data={'text': 'textey'}
         )
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)
