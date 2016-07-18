5b032d3f0794da901492aee1750f37bd03fb85dc
jackyhobingo <jackyhobingo@gmail.com>
2016-07-18 21:27:41 +0800

    Add new functional_tests which is error_messages_are_cleared_on_input

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -3,6 +3,8 @@ from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
 
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
 
     def test_cannot_add_empty_list_items(self):
         # Edith 前往首頁 並且不小心試著提交
@@ -12,7 +14,7 @@ class ItemValidationTest(FunctionalTest):
 
         # 首頁重新整理 有一個錯誤訊息
         # 說不能有空白的清單項目
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # 他再試一次 在項目中加入一些文字 現在可以動作了
@@ -24,7 +26,7 @@ class ItemValidationTest(FunctionalTest):
 
         # 他在清單網頁上看到類似的警告
         self.check_for_row_in_list_table('1: Buy milk')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You can't have an empty list item")
 
 
@@ -44,5 +46,19 @@ class ItemValidationTest(FunctionalTest):
 
         # 他看到一個有用的錯誤訊息
         self.check_for_row_in_list_table('1: Buy wellies')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You've already got this in your list")
+
+    def test_error_messages_are_cleared_on_input(self):
+        # Edith 再開始新的一個串列時, 會產生驗證錯誤
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('\n')
+        error = self.get_error_element()
+        self.assertTrue(error.is_displayed())
+
+        # 他開始在輸入方塊中打字，以清楚錯誤
+        self.get_item_input_box().send_keys('a')
+
+        # 他開心地看到錯誤訊息不見了
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())
