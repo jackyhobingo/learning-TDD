8bf371a157d0548ac9a1d2856902aa8ecd95b82b
jackyhobingo <jackyhobingo@gmail.com>
2016-07-18 16:16:07 +0800

    get ValidationError

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,11 +1,15 @@
+from unittest import skip
+
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.utils.html import escape
+
 from lists.models import Item, List
 from lists.views import home_page
 from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+
 
 class HomePageTest(TestCase):
 
@@ -60,8 +64,6 @@ class NewListTest(TestCase):
         self.assertIsInstance(response.context['form'], ItemForm)
 
 class ListViewTest(TestCase):
-
-
 
 
     def test_passes_correct_list_to_template(self):
@@ -146,3 +148,17 @@ class ListViewTest(TestCase):
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+    @skip
+    def test_duplicate_item_validation_error_on_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            '/lists/%d/' % (list1.id,),
+            data={'text': 'textey'}
+        )
+        expected_error = escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
+
+
