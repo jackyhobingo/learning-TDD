a9180f7667fe9cec296e43130933fbe0e3df5330
jackyhobingo <jackyhobingo@gmail.com>
2016-07-18 11:07:01 +0800

    rename all item input ids and names. still broken

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -5,9 +5,10 @@ from django.core.urlresolvers import resolve
 from django.utils.html import escape
 from lists.models import Item, List
 from lists.views import home_page
-from lists.forms import ItemForm
+from lists.forms import ItemForm, EMPTY_ITEM_ERROR
 
 class HomePageTest(TestCase):
+
 
     def test_home_page_renders_home_template(self):
         response = self.client.get('/')
@@ -17,7 +18,10 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertIsInstance(response.context['form'], ItemForm)
 
+
 class NewListTest(TestCase):
+
+
     def test_saving_a_POST_request(self):
 
         self.client.post(
@@ -48,7 +52,22 @@ class NewListTest(TestCase):
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
 
+
+    def test_for_invalid_input_renders_home_template(self):
+        response = self.client.post('/lists/new', data{'text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_validation_errors_are_shown_on_home_page(self):
+        response = self.client.post('/lists/new', data{'text': ''})
+        self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+    def test_for_invalid_input_passes_form_to_template(self):
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertIsInstance(response.context['form'], ItemForm)
+
 class ListViewTest(TestCase):
+
 
     def test_passes_correct_list_to_template(self):
         other_list = List.objects.create()
